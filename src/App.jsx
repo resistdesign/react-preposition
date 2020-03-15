75e2a5c756743f1f353b4bb436412159a1838cdb
Ryan Graff <ryantgraff@resistdesign.com>
2020-03-15 10:40:06 -0400

    feat: (App) Incorporate docu AppBase comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,26 @@
 import {hot} from 'react-hot-loader';
-import React, {Component, Fragment} from 'react';
-import styled, {createGlobalStyle} from 'styled-components';
+import React, {Component} from 'react';
+import styled from 'styled-components';
 import MyLayout, {
   HeaderSection,
   LeftPanel,
   RightPanel
 } from './App/MyLayout';
+import AppBase from './Common/ResistDesignPackageSiteLayout';
 
-const GlobalStyle = createGlobalStyle`
-  html,
-  body,
-  #app-root {
-    margin: 0;
-    padding: 0;
-  }
-`;
-const Base = styled.div`
-  width: 100vw;
-  min-height: 100vh;
-  flex: 1 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  justify-content: stretch;
-`;
 
 export class App extends Component {
   render() {
     return (
-      <Fragment>
-        <GlobalStyle/>
-        <Base>
-          <MyLayout
-            bgColor='#111111'
-          >
-            <HeaderSection>Header</HeaderSection>
-            <LeftPanel>Left</LeftPanel>
-            <RightPanel>Right</RightPanel>
-          </MyLayout>
-        </Base>
-      </Fragment>
+      <AppBase>
+        <MyLayout
+          bgColor='#111111'
+        >
+          <HeaderSection>Header</HeaderSection>
+          <LeftPanel>Left</LeftPanel>
+          <RightPanel>Right</RightPanel>
+        </MyLayout>
+      </AppBase>
     );
   }
 }
